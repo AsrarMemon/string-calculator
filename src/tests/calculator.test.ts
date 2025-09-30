bd2db9ae40a57429cd886df29cf7f087763605e5
Asrar Memon <asrar.memon0001@gmail.com>
2025-09-30 14:46:00 +0530

    test: handle new lines between numbers

--- src/tests/calculator.test.ts
+++ src/tests/calculator.test.ts
@@ -13,4 +13,8 @@ describe('String Calculator', () => {
     expect(add('1,2')).toBe(3);
   });
   
+  test('handle new lines between numbers', () => {
+    expect(add('1\n2,3')).toBe(6);
+  });
+
 });
